8053ba90c3c06556ba7726b0abf5c04f0f6a16fb
Selfrom <hlee314@gmail.com>
2016-08-08 15:59:26 -0700

    Add files via upload

---
+++ Game Demo 0.0.1/src/Map.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Map extends JPanel {
+
+}
---
+++ Game Demo 0.0.1/src/Satellite.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Satellite extends JComponent implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
