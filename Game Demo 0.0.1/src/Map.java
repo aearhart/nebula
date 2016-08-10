922ff082fe401c5e605628195f21529a1692ef2a
Adrienne Earhart <aearhart@hamilton.edu>
2016-08-10 11:31:11 -0700

    Now has basic implementation (1 sun, 1 water, 1 gas, 1 metal planet) with hover over highlight.

--- Game Demo 0.0.1/src/Map.java
+++ Game Demo 0.0.1/src/Map.java
@@ -6,16 +6,20 @@ public class Map extends JPanel {
 
 	public Map(){
 		this.setLayout(null);
-		Satellite s01 = new Satellite(0, 0);
-		Satellite s02 = new Satellite(30, 30);
-		Satellite s03 = new Satellite(800, 450);
+		Satellite s01 = new Satellite(450, 400); // sun 
+		Satellite s02 = new Satellite(1, 10, 40, 30); // water
+		Satellite s03 = new Satellite(2, 40, 600, 70); // gas
+		Satellite s04 = new Satellite(3, 700, 58, 47); // metal
+		
 		this.add(s01);
 		this.add(s02);
 		this.add(s03);
+		this.add(s04);
 		
-		s01.setBounds(0, 0, 100, 100);
-		s02.setBounds(30, 30, 100,100);
-		s03.setBounds(800, 450, 100, 100);
+		s01.setBounds(s01.getLocX(), s01.getLocY(), s01.getSz(), s01.getSz());
+		s02.setBounds(s02.getLocX(), s02.getLocY(), s02.getSz(), s02.getSz());
+		s03.setBounds(s03.getLocX(), s03.getLocY(), s03.getSz(), s03.getSz());
+		s04.setBounds(s04.getLocX(), s04.getLocY(), s04.getSz(), s04.getSz());
 	}
 	
 	@Override
--- Game Demo 0.0.1/src/Satellite.java
+++ Game Demo 0.0.1/src/Satellite.java
@@ -3,16 +3,49 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Satellite extends JComponent implements MouseListener{
+
+	private static final long serialVersionUID = 1L;
+	
 	private Integer x;
 	private Integer y;
+	private Integer s = 100; // size of sphere
 	private Color fillCol;
 	private Color borderCol;
+	private Color selectCol;
+	private Integer t = 0; // type: 0 = sun, 1 = water
 	
 	public Satellite(Integer locX, Integer locY) {
+		// default: sun
 		x = locX;
 		y = locY;
-		fillCol = Color.MAGENTA;
+		fillCol = Color.YELLOW;
 		borderCol = Color.BLACK;
+		selectCol = Color.ORANGE;
+		
+		addMouseListener(this);
+	}
+	
+	public Satellite(Integer ty, Integer locX, Integer locY, Integer sz) {
+		x = locX;
+		y = locY;
+		t = ty;
+		s = sz;
+		if (t == 1) { // water
+			fillCol = Color.BLUE;
+			borderCol = Color.BLACK;
+			selectCol = Color.CYAN;
+		}
+		else if (t == 2) { // gas
+			fillCol = Color.PINK;
+			borderCol = Color.BLACK;
+			selectCol = Color.RED;
+		}
+		
+		else if (t == 3) {
+			fillCol = Color.GRAY;
+			borderCol = Color.BLACK;
+			selectCol = Color.LIGHT_GRAY;
+		}
 		
 		addMouseListener(this);
 	}
@@ -25,15 +58,20 @@ public class Satellite extends JComponent implements MouseListener{
 		return y;
 	}
 	
+	public Integer getSz() {
+		return s+1;
+	}
+	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
 		g.setColor(fillCol);
-		g.fillOval(0, 0, 99, 99);	
+		g.fillOval(0, 0, s, s);	
 		g.setColor(borderCol);
-		g.drawOval(0, 0, 99, 99);
-		g.drawOval(0, 0, 98, 98);
+		g.drawOval(0, 0, s, s);
+		g.drawOval(0, 0, s-1, s-1);
+		g.drawOval(1, 1, s-1, s-1);
 	}
 	
 	@Override
@@ -54,12 +92,18 @@ public class Satellite extends JComponent implements MouseListener{
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		Color temp = borderCol;
+		borderCol = selectCol;
+		selectCol = temp;
+		repaint();
 	}
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-
+		Color temp = borderCol;
+		borderCol = selectCol;
+		selectCol = temp;
+		repaint();
 	}
 
 }
