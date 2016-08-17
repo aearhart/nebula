0df644c6c1cac6ce52422b55f6a378f9f96c4ebb
Harry Lee <hlee314@gmail.com>
2016-08-17 15:40:56 -0700

    Added Base. Added get/set methods to player

---
+++ GDT/src/Base.java
@@ -0,0 +1,35 @@
+import java.awt.Graphics;
+
+public class Base extends Satellite {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Planet[] planetsInAOI;
+	private Player controller;
+	
+	public Base(Integer locX, Integer locY, Integer sz) {
+		super(locX, locY, sz);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Planet[] getPInAOI(){
+		return planetsInAOI;
+	}
+	
+	public void setPInAOI(Planet[] planets) {
+		planetsInAOI = planets;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		g.setColor(fillCol);
+		g.fillOval(0, 0, s, s);	
+		g.setColor(borderCol);
+		g.drawOval(0, 0, s, s);
+	}
+
+}
--- GDT/src/Player.java
+++ GDT/src/Player.java
@@ -1,4 +1,54 @@
 
 public class Player {
+	private String name;
+	private Base[] bases;
+	private int gas;
+	private int water;
+	private int mineral;
+	
+	public Player(String n){
+		this.name = n;
+	}
+	
+	public void setBases(Base[] bs) {
+		this.bases = bs;
+	}
+	
+	public Base[] getBases() {
+		return bases;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWater() {
+		return water;
+	}
+
+	public void setWater(int water) {
+		this.water = water;
+	}
+
+	public int getMineral() {
+		return mineral;
+	}
+
+	public void setMineral(int mineral) {
+		this.mineral = mineral;
+	}
+	
+	public int getGas(){
+		return gas;
+	}
+
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
+	
+	
 }
