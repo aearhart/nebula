bcad9bb535d0450eb1e4fbaead8837d8699ab125
Harry Lee <hlee314@gmail.com>
2016-08-17 14:27:03 -0700

    Let's try this again

--- GDT/src/Controller.java
+++ GDT/src/Controller.java
@@ -4,6 +4,7 @@ public class Controller {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// hello world
+		// Harry's comment now
 		Window window = new Window();
 		/*Map map = new Map();
 		win1.add(map);
