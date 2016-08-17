bd481ceee213fe7622e2216e2569a559fecf7275
Harry Lee <hlee314@gmail.com>
2016-08-17 14:14:36 -0700

    New project

---
+++ GDT/src/Controller.java
@@ -0,0 +1,14 @@
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Window win1 = new Window();
+		/*Map map = new Map();
+		win1.add(map);
+		*/
+		
+	}
+
+}
