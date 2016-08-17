4015a21601addf6a827f86cb2413149863746422
Adrienne Earhart <aearhart@hamilton.edu>
2016-08-17 15:53:24 -0700

    Planets galore

--- GDT/src/Controller.java
+++ GDT/src/Controller.java
@@ -11,9 +11,9 @@ public class Controller {
 
 		// ??Planet(x, y, s, numResources, name)
 		Satellite s01 = new WaterPlanet(450, 400, 30, 3, "0");  
-		Satellite s02 = new WaterPlanet(800, 104, 40, 5, "1"); 
-		Satellite s03 = new WaterPlanet(645, 40, 26, 5, "2"); 
-		Satellite s04 = new WaterPlanet(766, 388, 58, 2, "3"); // metal
+		Satellite s02 = new MetalPlanet(800, 104, 40, 5, "1"); 
+		Satellite s03 = new GasPlanet(645, 40, 26, 5, "2"); 
+		Satellite s04 = new WaterPlanet(766, 388, 58, 2, "3"); 
 		
 		Satellite[] all = {s01, s02, s03, s04};
 		
--- GDT/src/WaterPlanet.java
+++ GDT/src/WaterPlanet.java
@@ -9,21 +9,21 @@ public class WaterPlanet extends Planet {
 		super(400, 400, 100);
 		// TODO Auto-generated constructor stub
 		this.setColors(Color.CYAN, Color.BLACK, Color.BLUE);
-		this.repaint();
+
 	}
 
 	public WaterPlanet(Integer locX, Integer locY, Integer sz, Integer numResources) {
 		super(locX, locY, sz, numResources);
 		// TODO Auto-generated constructor stub
 		this.setColors(Color.CYAN, Color.BLACK, Color.BLUE);
-		this.repaint();
+
 	}
 
 	public WaterPlanet(Integer locX, Integer locY, Integer sz, Integer numResources, String n) {
 		super(locX, locY, sz, numResources);
 		this.setName("Water Planet " + n);
 		this.setColors(Color.CYAN, Color.BLACK, Color.BLUE);
-		this.repaint();
+
 	}
 	
 }
