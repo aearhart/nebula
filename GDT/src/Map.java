70af58d84a3bfc5ee4943846d48ae8e761eb4845
Harry Lee <hlee314@gmail.com>
2016-08-29 14:04:12 -0700

    Added a simple background (bg)

--- GDT/src/Map.java
+++ GDT/src/Map.java
@@ -1,22 +1,37 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Map extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private Controller control;
+	private BufferedImage bg = null;
+	private int winsize = 1000;
 	
 	public Map(Controller ctrl){
 		control = ctrl;
 		this.setLayout(null);
-		this.setPreferredSize(new Dimension (1000,1000));
+		this.setPreferredSize(new Dimension (winsize, winsize));
 		
+		try {                
+			bg = ImageIO.read(getClass().getResource("bg.png"));
+		} catch (IOException ex) {
+			// handle exception...
+			System.out.println("IO EXCEPTION: "+ ex);
+		}
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
+		g.drawImage(bg, 0, 0, winsize, winsize, null);
+		
 		g.setColor(control.AoIc);
 		g.drawOval(control.AoIx, control.AoIy, control.AoIs, control.AoIs);
+		
 		
 		g.setColor(Color.BLACK);
 		for(int i = 0; i < 11; i++)
