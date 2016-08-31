1379feb3dae1d9ffc7dfbe22607837b7e77b5110
Harry Lee <hlee314@gmail.com>
2016-08-31 15:40:02 -0700

    Updated ip

--- GD 0.0.2/src/ClientTest.java
+++ GD 0.0.2/src/ClientTest.java
@@ -9,7 +9,8 @@ public class ClientTest {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Connecting...");
 //		socket = new Socket("localhost", 7300);
-		socket = new Socket("70.95.122.247", 7777);
+//		socket = new Socket("70.95.122.247", 7777);
+		socket = new Socket("2605:e000:1c02:8e:9d7f:687b:3d6:73f5", 7777);
 		System.out.println("Connection established.");
 		in = new DataInputStream(socket.getInputStream());
 		System.out.println("Receiving information...");
