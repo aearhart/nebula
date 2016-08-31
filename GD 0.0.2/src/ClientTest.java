c1224ffaa795f4b331e1442d806bc55afe688ba9
Harry Lee <hlee314@gmail.com>
2016-08-31 15:02:26 -0700

    Updated with print statements

--- GD 0.0.2/src/ClientTest.java
+++ GD 0.0.2/src/ClientTest.java
@@ -7,8 +7,11 @@ public class ClientTest {
 	static DataInputStream in;
 	
 	public static void main(String[] args) throws Exception {
-		socket = new Socket("172.16.42.2", 7777);
+		System.out.println("Connecting...");
+		socket = new Socket("172.16.42.2", 7778);
+		System.out.println("Connection established.");
 		in = new DataInputStream(socket.getInputStream());
+		System.out.println("Receiving information...");
 		String s = in.readUTF();
 		System.out.println("Message from server: " + s);
 
--- GD 0.0.2/src/ServerTest.java
+++ GD 0.0.2/src/ServerTest.java
@@ -8,10 +8,14 @@ public class ServerTest {
 	static DataOutputStream out;
 	
 	public static void main(String[] args) throws Exception {
-		serverSocket = new ServerSocket(7777);
+		System.out.println("Starting server...");
+		serverSocket = new ServerSocket(7778);
+		System.out.println("Server started.");
 		socket = serverSocket.accept();
+		System.out.println("Connection from: " + socket.getInetAddress());
 		out = new DataOutputStream(socket.getOutputStream());
 		out.writeUTF("Heyyyy");
+		System.out.println("Sent message.");
 	}
 
 }
