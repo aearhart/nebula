7f5cbdcff285a03fa55ca1ec3335543e10ae49bd
Adrienne Earhart <aearhart@hamilton.edu>
2016-09-01 16:51:23 -0700

    Two clients + one server, changing letters by taking turns

--- GD 0.0.2/src/ClientTest.java
+++ GD 0.0.2/src/ClientTest.java
@@ -1,22 +1,101 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ClientTest {
 
 	static Socket socket;
 	static DataInputStream in;
+	static DataOutputStream out;
+	private static String s;
+	private static JFrame f;
+	private static JButton button;
+	private static JPanel panel;
+	private static Boolean click = true;
+	
+	public static String getMessage() throws IOException  {
+		in = new DataInputStream(socket.getInputStream());
+		s = in.readUTF();
+		System.out.println("Receiving information...  " + s);
+
+		return s;
+	}
+	
+	public static void sendMessage(String s) throws IOException {
+		out = new DataOutputStream(socket.getOutputStream());
+		out.writeUTF(s);
+		System.out.println("Message sent: " + s);
+		}
 	
 	public static void main(String[] args) throws Exception {
 		System.out.println("Connecting...");
-//		socket = new Socket("localhost", 7300);
+		socket = new Socket("localhost", 7777); // local server
 //		socket = new Socket("70.95.122.247", 7777);
-		socket = new Socket("2605:e000:1c02:8e:9d7f:687b:3d6:73f5", 7777);
+//		socket = new Socket("2605:e000:1c02:8e:9d7f:687b:3d6:73f5", 7777); // computer server
 		System.out.println("Connection established.");
-		in = new DataInputStream(socket.getInputStream());
-		System.out.println("Receiving information...");
-		String s = in.readUTF();
+		
+		/*
+		getMessage();
 		System.out.println("Message from server: " + s);
 
+		sendMessage("Hi back.");
+		*/
+		getMessage();
+		// frame setup
+		f = new JFrame("testing sockets");
+		f.setVisible(true);
+		f.setSize(500, 200);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		panel = new JPanel();
+		f.add(panel);
+		button = new JButton(s);
+		panel.add(button);
+		button.addActionListener(new Action1());
+		
+		
+	}
+	
+	public static String addOne(String s) {
+		char value = s.charAt(0);
+		char nextValue = (char)((int)value + 1);
+		s = "";
+		s += nextValue;
+		return s;
+	}
+	
+	static class Action1 implements ActionListener {
+		@Override
+		public void actionPerformed (ActionEvent e) {
+			if ((int)s.charAt(0) >= 'Z') {
+				click = false;
+				button.setText("Done");
+			}
+			if (click) {
+			try {
+				sendMessage(addOne(s));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				getMessage();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			button.setText(s);
+			}
+			else {
+				
+			}
+		}
 	}
 
 }
--- GD 0.0.2/src/ServerTest.java
+++ GD 0.0.2/src/ServerTest.java
@@ -5,7 +5,50 @@ public class ServerTest {
 
 	static ServerSocket serverSocket;
 	static Socket socket;
+	static Socket socket2;
 	static DataOutputStream out;
+	static DataInputStream in;
+	private static String s = "A";
+	private static String prev = "A";
+	
+	public static String getMessage(Socket sock) throws IOException  {
+		in = new DataInputStream(sock.getInputStream());
+		s = in.readUTF();
+		System.out.println("Receiving information...  " + s);
+		return s;
+	}
+	
+	public static void sendMessage(Socket sock, String s) throws IOException {
+		out = new DataOutputStream(sock.getOutputStream());
+		out.writeUTF(s);
+		System.out.println("Sent message: " + s);
+	}
+	
+	public static String addOne(String s) {
+		char value = s.charAt(0);
+		char nextValue = (char)((int)value + 1);
+		s = "";
+		s += nextValue;
+		return s;
+	}
+	
+	public static Boolean viable() {
+		char orig = prev.charAt(0);
+		char possible = s.charAt(0);
+//		System.out.println(orig + " <= " + possible + " ?");
+		if (((int)orig) >= ((int)possible)) {
+			s = prev;
+			return false;
+		}
+		return true;
+	}
+	
+	public static Boolean stillGo() {
+		char orig = prev.charAt(0);
+		if (((int)orig) >= ((int)'Z'))
+			return false;
+		return true;
+	}
 	
 	public static void main(String[] args) throws Exception {
 		System.out.println("Starting server...");
@@ -13,9 +56,50 @@ public class ServerTest {
 		System.out.println("Server started.");
 		socket = serverSocket.accept();
 		System.out.println("Connection from: " + socket.getInetAddress());
-		out = new DataOutputStream(socket.getOutputStream());
-		out.writeUTF("Heyyyy");
-		System.out.println("Sent message.");
+		
+		socket2 = serverSocket.accept(); // 2nd client
+		System.out.println("Connection from: " + socket2.getInetAddress());	
+		/*out = new DataOutputStream(socket.getOutputStream());
+		out.writeUTF("Heyyyy");*/
+		/*
+		sendMessage("Hi there.");
+
+		
+		getMessage();
+		System.out.println(s);
+		*/
+		
+		sendMessage(socket, s);
+		sendMessage(socket2, s); // 2nd client
+		
+		while(stillGo()) {
+			getMessage(socket);
+			if (viable()) {
+				s = addOne(s);
+				prev = s;
+				sendMessage(socket, s);
+			}
+			else
+				sendMessage(socket, s);
+			
+			getMessage(socket2);         // 2nd client
+			if (viable()) {
+				s = addOne(s);
+				prev = s;
+				sendMessage(socket2, s);
+			}
+			else
+				sendMessage(socket2, s);  /// end of second client
+		}
+		
+		getMessage(socket);
+		sendMessage(socket, "1");
+		
+		getMessage(socket2);      /// 2nd client
+		sendMessage(socket2, "1"); // end of 2nd client
+		
+		System.out.println("Done");
 	}
+	
 
 }
